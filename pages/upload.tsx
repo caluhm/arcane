aa3a574c7031bc12c1ff4bdfc16570c5cc34c91d
caluhm <calum.jarvo@gmail.com>
2023-01-12 23:28:22 +0000

    Added post detail page when you click a post with play, mute and return to home page buttons/functionality.

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -9,6 +9,7 @@ import useAuthStore from '../store/authStore';
 import { client } from '../utils/client';
 
 import { topics } from '../utils/constants';
+import { BASE_URL } from '../utils';
 
 const Upload = () => {
 
@@ -64,7 +65,7 @@ const Upload = () => {
             topic: game
         }
 
-        await axios.post('http://localhost:3000/api/post', document);
+        await axios.post(`${BASE_URL}/api/post`, document);
 
         router.push('/');
     }

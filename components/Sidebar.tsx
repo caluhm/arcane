71882e233597ed4c814513f9adefe892c98384a2
caluhm <calum.jarvo@gmail.com>
2023-01-12 21:23:19 +0000

    Created an upload video page and allowed videos to be successfully uploaded to sanity

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -39,29 +39,6 @@ const Sidebar = () => {
                         </div>
                     </Link>
                 </div>
-                {!userProfile && (
-                    <div className='px-2 py-4 hidden xl:block'>
-                        <p className='text-gray-400'>Log in to like and comment on videos</p>
-                        <div className='pr-4'>
-                            <GoogleLogin 
-                                clientId=''
-                                render={(renderProps) => (
-                                    <button
-                                        className='cursor-pointer bg-white text-lg text-[#40b7de] border-[1px] border-[#40b7de] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#40b7de]'
-                                        onClick={renderProps.onClick}
-                                        disabled={renderProps.disabled}
-                                    >
-                                        Log in
-                                    </button>
-                                )}  
-                                onSuccess={() => []}
-                                onFailure={() => []}
-                                cookiePolicy={'single_host_origin'}
-                            />
-                        </div>
-                    </div>
-                )}
-
                 <Discover />
                 <SuggestedAccounts />
                 <Footer />

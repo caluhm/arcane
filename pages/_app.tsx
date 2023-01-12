8e1fc3ba3b276dd7996499b7b7cccd831b8cf110
caluhm <calum.jarvo@gmail.com>
2023-01-12 19:04:59 +0000

    Created user authentication using google oauth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import '../styles/globals.css'
+import { GoogleOAuthProvider  } from '@react-oauth/google'
 
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
@@ -16,7 +17,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if(isSSR) return null;
 
   return (
-    <div>
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
       <Navbar />
       <div className='flex gap-6 md:gap-20'>
         <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
@@ -26,8 +27,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </div>
       </div>
-      
-    </div>
+    </GoogleOAuthProvider>
   );
 }
 
